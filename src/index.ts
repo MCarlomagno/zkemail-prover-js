ec8549d4ea0787245e1a4b76997c5560395a50bf
Marcos <marcoscarlomagno1@gmail.com>
2025-05-08 11:04:44 -0300

    prove recovery email

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-const world = 'world';
+import zke from "@zk-email/sdk";
+import * as fs from 'fs';
+import * as path from 'path';
 
-export function main() {
-  console.log(`Hello ${world}! `);
+function log(str: string) {
+    console.log(`[ZkEmail Prover] ${str}`);
 }
 
-main();
+async function main() {
+  const zkemail = zke();
+
+  log("Loading blueprint");
+  const blueprint = await zkemail.getBlueprint("MCarlomagno/openzeppelin_recovery@v1");
+
+  const emailFilePath = path.join(__dirname, 'fixtures', 'Recover.eml');
+  const emailContent: string = fs.readFileSync(emailFilePath, 'utf-8');
+
+  log("creating local prover");
+  const prover = blueprint.createProver({ isLocal: false });
+
+  log("proving");
+  const proof = await prover.generateLocalProof(emailContent);
+
+  log("verifying");
+  const verified = await blueprint.verifyProof(proof);
+
+  log(`Proof is valid: ${verified}`);
+}
+
+main()
+    .then(() => log("done"))
+    .catch((e) => console.error(e));
